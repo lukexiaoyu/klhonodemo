10b06e4f71dd6fc282664ec9afa623d8f9f2c1ce
lukexiao <814008000@163.com>
2023-10-04 11:31:38 +0800

    1

--- netlify/edge-functions/index.ts
+++ netlify/edge-functions/index.ts
@@ -3,8 +3,13 @@ import { Hono } from 'https://deno.land/x/hono@v3.7.2/mod.ts'
 
 const app = new Hono()
 
+app.get('/',(c)=>{
+  return c.html(`<h1 style='color:red;'>klpalace</h1>`)
+})
+
 app.get('/country', (c) => {
   return c.text('Hello Hono!')
 })
+
 
 export default handle(app)
